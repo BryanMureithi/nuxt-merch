a5ed8763047179f5e53c209b741bac9f241f454a
BryanMureithi <brymureithi@gmail.com>
2024-03-06 23:21:39 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <main>
+      <div>
+        <h2>This is the Home page</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero fugit
+          perferendis perspiciatis qui, commodi amet labore error eius tenetur
+          voluptas!
+        </p>
+      </div>
+    </main>
+  </div>
+</template>
